6cc8bfe0dcaf6bc5b9c1b00b6cfdb4ae64249a0c
Matt Draghi <mtd5264@gmail.com>
2022-01-17 11:59:50 -0500

    commit tree af70c0c7c32ed12da74ab6443fa3cc6b358ef348

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () =>{
+    let text = document.querySelector('p#text')
+    console.log(text)
+    text.textContent = 'This is really cool!'
+})
